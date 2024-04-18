d093c2aa15265a1ce6290d380e98fe922fd951c9
Joaquim Rafael Prieto <joaquim.mpp@outlook.com>
2024-04-18 15:34:22 -0300

    Update projProcessos.c

--- Projeto1/projProcessos.c
+++ Projeto1/projProcessos.c
@@ -1,94 +1,109 @@
-#include <stdlib.h>
+#include <fcntl.h>
 #include <malloc.h>
-#include <sys/types.h>
-#include <sys/mman.h>
-#include <sys/wait.h>
-#include <signal.h>
 #include <sched.h>
+#include <semaphore.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <wait.h>
-#include <semaphore.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #define _GNU_SOURCE
 
 typedef struct {
-    int tempo, direcao;
-}Passageiro;
+  int tempo, direcao;
+} Passageiro;
 
-void calculaTempoPassageiro(int chegada,int dir,Passageiro *passageiro,sem_t *semaforo);
-
-int criarProcessosFilhos(FILE *arquivo,int numPassageiros,Passageiro *passageiro,sem_t *semaforo);
-
-int main(){
-    int numPassageiros;
-    FILE* arquivo = fopen("./input/E_5", "r");
-    if(arquivo==NULL){
-        printf("Erro ao abrir arquivo!");
-        exit(1);
-    }
-    fscanf(arquivo, "%d", &numPassageiros);
-    int shm = shm_open("/shm", O_CREAT | O_RDWR, 0666);
-    ftruncate(shm, sizeof(Passageiro));
-    Passageiro* passageiro = (Passageiro*)mmap(0,sizeof(Passageiro),PROT_READ|PROT_WRITE, MAP_SHARED,shm,0);
-    passageiro->direcao = -1;passageiro->tempo = 0;
-    close(shm);
-
-    sem_t* semaforo = sem_open("/semaforo", O_CREAT, 0666, 1);
-
-    criarProcessosFilhos(arquivo,numPassageiros,passageiro,semaforo);
-
-    printf("O momento final de parada da escada rolante é %d\n",passageiro->tempo);
-
-    fclose(arquivo);
-    shm_unlink("/shm");
-    munmap(passageiro, sizeof(Passageiro));
-    close(shm);
-    sem_close(semaforo);
-    sem_unlink("/semaforo");
-    return 0;
+void lerPassageiros(char *caminhoArquivo, Passageiro *passageiros,
+                    int *numPassageiros) {
+  FILE *arquivo = fopen(caminhoArquivo, "r");
+  if (arquivo == NULL) {
+    printf("Erro ao abrir o arquivo.\n");
+    exit(1);
+  }
+  fscanf(arquivo, "%d", numPassageiros);
+  for (int i = 0; i < *numPassageiros; i++) {
+    fscanf(arquivo, "%d %d", &passageiros[i].tempo, &passageiros[i].direcao);
+  }
+  fclose(arquivo);
 }
 
-int criarProcessosFilhos(FILE *arquivo,int numPassageiros,Passageiro *passageiro,sem_t *semaforo){
-    pid_t pid;
-    pid_t* child_pids = malloc(numPassageiros*sizeof(pid_t));
+int ultimoMomentoEscada(Passageiro *passageiros, int numPassageiros) {
+  int filaEsperando = 0;
+  int tempoFinal = 0;
+  int direcao = -1;
+  int i;
 
-    for(int i=0;i<numPassageiros;i++){
-        int tempoChegada,direcao;
-        fscanf(arquivo, "%d %d", &tempoChegada, &direcao);
-        pid = fork();
-        if(pid == 0){
-            calculaTempoPassageiro(tempoChegada,direcao,passageiro,semaforo);
-            exit(0);
-        }else if(pid<0) {
-            printf("Erro ao criar processo filho.\n");
-            exit(1);
-        }else{
-            child_pids[i] = pid;
+  for (i = 0; i < numPassageiros; i++) {
+    if (direcao == -1) {
+      direcao = passageiros[i].direcao;
+      tempoFinal = passageiros[i].tempo + 10;
+
+    } else if (passageiros[i].direcao == direcao) {
+      if (filaEsperando > 0 && passageiros[i].tempo > tempoFinal) {
+        filaEsperando = 1;
+        tempoFinal += 10;
+        direcao = passageiros[i].direcao * (-1);
+      } else {
+        direcao = passageiros[i].direcao;
+        tempoFinal = passageiros[i].tempo + 10;
+      }
+    } else {
+      if (passageiros[i].tempo > tempoFinal) {
+        if (filaEsperando > 0) {
+          tempoFinal += 10;
+          filaEsperando = 1;
+        } else {
+          tempoFinal = passageiros[i].tempo + 10;
         }
+      } else {
+        filaEsperando++;
+      }
     }
+  }
+  if (filaEsperando > 0) {
+    tempoFinal += 10;
+  }
 
-    for(int i=0;i<numPassageiros;i++){
-      waitpid(child_pids[i], NULL, 0);
-    }
-
-    free(child_pids);
-    return 0;
+  return tempoFinal;
 }
 
-void calculaTempoPassageiro(int chegada,int dir,Passageiro *passageiro,sem_t *semaforo){
-    sleep(chegada);
+int main() {
+  Passageiro passageiros[10000];
+  int numPassageiros;
 
-    sem_wait(semaforo);
-    if(passageiro->direcao == -1||passageiro->direcao == dir){
-        passageiro->direcao = dir;passageiro->tempo = chegada + 10;
-    }else{
-      sem_post(semaforo);
-      sleep(passageiro->tempo-chegada);
-      sem_wait(semaforo);
-      passageiro->direcao = dir;
-      passageiro->tempo = chegada + 10;
-    }
-    sem_post(semaforo);
+  lerPassageiros("./input/E_5", passageiros, &numPassageiros);
+
+  int fd[2]; 
+
+  if (pipe(fd) == -1) {
+    perror("Erro ao criar pipe");
+    exit(EXIT_FAILURE);
+  }
+
+  pid_t pid = fork();
+
+  if (pid == -1) {
+    perror("Erro ao criar processo filho");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) {
+    close(fd[0]);
+    int tempoFinal = ultimoMomentoEscada(passageiros, numPassageiros);
+    write(fd[1], &tempoFinal, sizeof(int));
+    close(fd[1]);
+    exit(EXIT_SUCCESS);
+  } else {
+    close(fd[1]);
+    int resultado;
+    wait(NULL);
+    read(fd[0], &resultado, sizeof(int));
+    close(fd[0]);
+
+  printf("O momento final da escada rolante eh %d ", resultado);
+
+  return 0;
+}
 }
