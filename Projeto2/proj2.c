28b972e496ddeb75a98794321d6eb129cbe17d8b
Cloud User <ec2-user@ip-172-31-41-98.ec2.internal>
2024-05-09 19:57:54 +0000

    Proj2

--- Projeto2/proj2.c
+++ Projeto2/proj2.c
@@ -1,58 +1,128 @@
 #define _GNU_SOURCE
+#include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include <sched.h>
-#include <stdio.h>
-// 64kB stack
+#include <semaphore.h>
+
 #define FIBER_STACK 1024*64
 
 struct c {
-int saldo;
+    int saldo;
 };
 typedef struct c conta;
 
 conta from, to;
 int valor;
+int total_transferencias = 0; 
 
-// The child thread will execute this function
-int transferencia( void *arg){
-    if (from.saldo >= valor){ // 2
-    from.saldo -= valor;
-    to.saldo += valor;
+sem_t semaforo_from;
+sem_t semaforo_to;
+
+void* transferenciaFromTo(void *arg) {
+
+  sem_wait(&semaforo_from);
+  sem_wait(&semaforo_to);
+
+  if (from.saldo >= valor) {
+      from.saldo -= valor;
+      to.saldo += valor;
+      printf("Transferência %d concluída.\n", total_transferencias + 1);
+      printf("Saldo de from: %d\n", from.saldo);
+      printf("Saldo de to: %d\n", to.saldo);
+      total_transferencias++;
   }
-  printf("Transferência concluída com sucesso!\n");
-  printf("Saldo de c1: %d\n", from.saldo);
-  printf("Saldo de c2: %d\n", to.saldo);
-  return 0;
+
+  sem_post(&semaforo_to);
+  sem_post(&semaforo_from);
+
+  return NULL;
 }
-int main(){
-  void* stack;
+
+void* transferenciaToFrom(void *arg) {
+
+  sem_wait(&semaforo_to);
+  sem_wait(&semaforo_from);
+
+  if (to.saldo >= valor) {
+      to.saldo -= valor;
+      from.saldo += valor;
+      printf("Transferência %d concluída.\n", total_transferencias + 1);
+      printf("Saldo de to: %d\n", to.saldo);
+      printf("Saldo de from: %d\n", from.saldo);
+      total_transferencias++;
+  }
+
+  sem_post(&semaforo_from);
+  sem_post(&semaforo_to);
+
+  return NULL;
+}
+
+int main() {
+  void *stack;
   pid_t pid;
   int i;
-  // Allocate the stack
-  stack = malloc( FIBER_STACK );
-  if ( stack == 0 ){
-    perror("malloc: could not allocate stack");
-    exit(1);
-  }
-  // Todas as contas começam com saldo 100
+  int option;
+
+  sem_init(&semaforo_from, 0, 1);
+  sem_init(&semaforo_to, 0, 1);
+
   from.saldo = 100;
   to.saldo = 100;
-  printf( "Transferindo 10 para a conta c2\n" );
+
+  printf("Escolha uma opção(0= From -> To, 1= To -> From):\n");
+  scanf("%d",&option);
+
+  printf("Transferindo 10 entre as contas from e to\n");
   valor = 10;
-  for (i = 0; i < 10; i++) {
-    // Call the clone system call to create the child thread
-    pid = clone( &transferencia, (char*) stack + FIBER_STACK,SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, 0 );
-    if ( pid == -1 ){
-      perror( "clone" );
-      exit(2);
+
+  if(option == 0){
+
+    for (i = 0; i < 100; i++) {
+        stack = malloc(FIBER_STACK);
+        if (stack == 0) {
+            perror("malloc: could not allocate stack");
+            exit(1);
+        }
+
+        pid = clone((void*) &transferenciaFromTo, (char *)stack + FIBER_STACK, SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, NULL);
+        if (pid == -1) {
+            perror("clone");
+            exit(2);
+        }
+    }
+  }else{
+    for (i = 0; i < 100; i++) {
+        stack = malloc(FIBER_STACK);
+        if (stack == 0) {
+            perror("malloc: could not allocate stack");
+            exit(1);
+        }
+
+        pid = clone((void*) &transferenciaToFrom, (char *)stack + FIBER_STACK, SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, NULL);
+        if (pid == -1) {
+            perror("clone");
+            exit(2);
+        }
     }
   }
-  // Free the stack
-  free( stack );
+
+  for (i = 0; i < 10; i++) {
+        wait(NULL);
+  }
+
+  free(stack);
+
+  sem_destroy(&semaforo_from);
+  sem_destroy(&semaforo_to);
+
   printf("Transferências concluídas e memória liberada.\n");
+  printf("Saldo final de from: %d\n", from.saldo);
+  printf("Saldo final de to: %d\n", to.saldo);
+
   return 0;
 }
