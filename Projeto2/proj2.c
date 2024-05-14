afc5b1f119ccd3e3f3252a3b0d86b5efab293beb
Cloud User <ec2-user@ip-172-31-41-98.ec2.internal>
2024-05-14 15:52:36 +0000

    Proj2 Final

--- Projeto2/proj2.c
+++ Projeto2/proj2.c
@@ -1,14 +1,11 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <sched.h>
+#include <unistd.h>
+#include <pthread.h>
 #include <semaphore.h>
 
-#define FIBER_STACK 1024*64
+#define FIBER_STACK (1024*64)
 
 struct c {
     int saldo;
@@ -17,112 +14,82 @@ typedef struct c conta;
 
 conta from, to;
 int valor;
-int total_transferencias = 0; 
+int valor1;
+int total_transferencias = -1; 
 
-sem_t semaforo_from;
-sem_t semaforo_to;
+sem_t semaforo;
 
 void* transferenciaFromTo(void *arg) {
+    sem_wait(&semaforo);
+    total_transferencias++;
+    if (from.saldo >= valor) {
+        from.saldo -= valor;
+        to.saldo += valor;
+        printf("Transferência %d concluída from->to.\n", total_transferencias + 1);
+        printf("Saldo de from: %d\n", from.saldo);
+        printf("Saldo de to: %d\n", to.saldo);
+    } else {
+        printf("Transferência %d falhou: saldo insuficiente em 'from'.\n", total_transferencias );
+    }
+    
+    sem_post(&semaforo);
 
-  sem_wait(&semaforo_from);
-  sem_wait(&semaforo_to);
-
-  if (from.saldo >= valor) {
-      from.saldo -= valor;
-      to.saldo += valor;
-      printf("Transferência %d concluída.\n", total_transferencias + 1);
-      printf("Saldo de from: %d\n", from.saldo);
-      printf("Saldo de to: %d\n", to.saldo);
-      total_transferencias++;
-  }
-
-  sem_post(&semaforo_to);
-  sem_post(&semaforo_from);
-
-  return NULL;
+    pthread_exit(NULL);
 }
 
 void* transferenciaToFrom(void *arg) {
+    sem_wait(&semaforo);
+    total_transferencias++;
+    if (to.saldo >= valor1) {
+        to.saldo -= valor1;
+        from.saldo += valor1;
+        printf("Transferência %d concluída to->from.\n", total_transferencias + 1);
+        printf("Saldo de to: %d\n", to.saldo);
+        printf("Saldo de from: %d\n", from.saldo);
+    } else {
+        printf("Transferência %d falhou: saldo insuficiente em 'to'.\n", total_transferencias);
+    }
 
-  sem_wait(&semaforo_to);
-  sem_wait(&semaforo_from);
+    sem_post(&semaforo);
 
-  if (to.saldo >= valor) {
-      to.saldo -= valor;
-      from.saldo += valor;
-      printf("Transferência %d concluída.\n", total_transferencias + 1);
-      printf("Saldo de to: %d\n", to.saldo);
-      printf("Saldo de from: %d\n", from.saldo);
-      total_transferencias++;
-  }
-
-  sem_post(&semaforo_from);
-  sem_post(&semaforo_to);
-
-  return NULL;
+    pthread_exit(NULL);
 }
 
 int main() {
-  void *stack;
-  pid_t pid;
-  int i;
-  int option;
+    pthread_t threads[100];
+    int i;
 
-  sem_init(&semaforo_from, 0, 1);
-  sem_init(&semaforo_to, 0, 1);
+    sem_init(&semaforo, 0, 1);
 
-  from.saldo = 100;
-  to.saldo = 100;
+    from.saldo = 100;
+    to.saldo = 100;
+    valor = 0;
+    valor1 = 100;
 
-  printf("Escolha uma opção(0= From -> To, 1= To -> From):\n");
-  scanf("%d",&option);
-
-  printf("Transferindo 10 entre as contas from e to\n");
-  valor = 10;
-
-  if(option == 0){
-
-    for (i = 0; i < 100; i++) {
-        stack = malloc(FIBER_STACK);
-        if (stack == 0) {
-            perror("malloc: could not allocate stack");
+    for (i = 0; i < 50; i++) {
+        if (pthread_create(&threads[i], NULL, transferenciaFromTo, NULL)) {
+            perror("pthread_create");
             exit(1);
         }
-
-        pid = clone((void*) &transferenciaFromTo, (char *)stack + FIBER_STACK, SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, NULL);
-        if (pid == -1) {
-            perror("clone");
-            exit(2);
-        }
-    }
-  }else{
-    for (i = 0; i < 100; i++) {
-        stack = malloc(FIBER_STACK);
-        if (stack == 0) {
-            perror("malloc: could not allocate stack");
+        if (pthread_create(&threads[i+50], NULL, transferenciaToFrom, NULL)) {
+            perror("pthread_create");
             exit(1);
         }
+    }
 
-        pid = clone((void*) &transferenciaToFrom, (char *)stack + FIBER_STACK, SIGCHLD | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_VM, NULL);
-        if (pid == -1) {
-            perror("clone");
-            exit(2);
+    for (i = 0; i < 50; i++) {
+        if (pthread_join(threads[i], NULL)) {
+            perror("pthread_join");
+            exit(1);
         }
     }
-  }
 
-  for (i = 0; i < 10; i++) {
-        wait(NULL);
-  }
+    sem_destroy(&semaforo);
 
-  free(stack);
+    printf("Transferências concluídas e memória liberada.\n");
+    printf("Saldo final de from: %d\n", from.saldo);
+    printf("Saldo final de to: %d\n", to.saldo);
 
-  sem_destroy(&semaforo_from);
-  sem_destroy(&semaforo_to);
-
-  printf("Transferências concluídas e memória liberada.\n");
-  printf("Saldo final de from: %d\n", from.saldo);
-  printf("Saldo final de to: %d\n", to.saldo);
-
-  return 0;
+    return 0;
 }
+
